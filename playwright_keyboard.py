f20f9dc9c879bf06479c6e9b711857cd3eefa3a9
Evgen <163992459+evgeniy1331@users.noreply.github.com>
2026-02-19 19:07:55 +0300

    5.5 Playwright

---
+++ playwright_keyboard.py
@@ -0,0 +1,18 @@
+
+
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
+
+    input_email = page.get_by_test_id('login-form-email-input').locator('input')
+    input_email.focus()
+
+    for char in 'user@gmail.com':
+        page.keyboard.type(char, delay=300)
+
+    page.keyboard.press('ControlOrMeta+A')
+
+    page.wait_for_timeout(3000)
