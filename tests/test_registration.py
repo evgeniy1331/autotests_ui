814cdbe347f101cb9af6d222d621c93870957861
Evgen <163992459+evgeniy1331@users.noreply.github.com>
2026-02-24 18:48:45 +0300

    playwright and pytest

---
+++ tests/test_registration.py
@@ -0,0 +1,30 @@
+from playwright.sync_api import sync_playwright
+
+def test_successful():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        email_input.fill('test_user@gmail.com')
+
+        user_name_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        user_name_input.fill('test_user')
+
+        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        password_input.fill('123456789')
+
+        registration_button = page.get_by_test_id('registration-page-registration-button')
+        registration_button.click()
+
+        context.storage_state(path='browser-state.json')
+
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context(storage_state='browser-state.json')
+        page = context.new_page()
+
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
