ed1c1b981f858e46f0e6beb7c9ec074e27ad2824
Evgen <163992459+evgeniy1331@users.noreply.github.com>
2026-02-25 15:42:09 +0300

    default marks pytest

--- tests/test_pytest_xfail.py
+++ tests/test_pytest_xfail.py
@@ -1,9 +1,9 @@
 import pytest
 
-@pytest.mark.xfail
+@pytest.mark.xfail(reason="Найдет баг в приложении, из-за которого тест падает")
 def test_with_bug():
     assert 1 == 2
 
-@pytest.mark.xfail
+@pytest.mark.xfail(reason="Баг уже устранен, но на тествсе еше висит маркировка xfail")
 def test_without_bug():
     ...
